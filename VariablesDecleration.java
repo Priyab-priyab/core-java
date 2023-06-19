1e4ff492574c602a2b8b860f62a2b1e5c314f0d1
unknown <you@example.com>
2023-06-19 22:28:29 +0530

    First commit

---
+++ VariablesDecleration.java
@@ -0,0 +1,43 @@
+public class VariablesDecleration{
+	public static void main(String[] args){
+		
+		byte MyAge=20;
+		MyAge=21;
+		System.out.println(MyAge);
+		
+		short MyUsn=060;
+		MyUsn=69;
+		System.out.println(MyUsn);
+		
+		int MyPhoneNumber=1945474846;
+		MyPhoneNumber=1296296466;
+		System.out.println(MyPhoneNumber);
+		
+		long MyAdharNumber=258369147258L;
+		MyAdharNumber=369258147369L;
+		System.out.println(MyAdharNumber);
+		
+		float MyDegreePercentage=80.5F;
+		MyDegreePercentage=85F;
+		System.out.println(MyDegreePercentage);
+		
+		double MySslcPercentage=90.45874;
+		MySslcPercentage=87.2645;
+		System.out.println(MySslcPercentage);
+		
+		char MyBloodGroup='A';
+		MyBloodGroup='A';
+        System.out.println(MyBloodGroup);    
+		
+		boolean IAmGood=false;
+		IAmGood=true;
+		System.out.println(IAmGood);
+		
+		String MyName = "priya b";
+		MyName = "vanitha B";
+		System.out.println(MyName);
+	
+	
+	}
+}
+
