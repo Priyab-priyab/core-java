833ca98a3d77245d34eee7d99856d386e23790ee
unknown <you@example.com>
2023-06-21 11:14:46 +0530

    third

---
+++ ArrayDecleration.java
@@ -0,0 +1,34 @@
+public class ArrayDecleration
+{
+	public static void main(String[] args)
+	{
+		int[] noOfStudents={10,20,30,50,80};
+			{
+				systm.out.println(noOfStudents[0]);
+				systm.out.println(noOfStudents[1]);
+				systm.out.println(noOfStudents[2]);
+				systm.out.println(noOfStudents[3]);
+				systm.out.println(noOfStudents[4]);
+		    }
+		
+		char[] vowels={'a','e','i','o','u'};
+			{
+				systm.out.println(vowels[0]);
+				systm.out.println(vowels[1]);
+				systm.out.println(vowels[2]);
+				systm.out.println(vowels[3]);
+				systm.out.println(vowels[4]);
+			}
+		
+        float[] heightOfStudents = {6.2f , 3.5f , 5.2f , 4.8f};
+			{
+       
+				systm.out.println(heightOfStudents[0]);
+				systm.out.println(heightOfStudents[1]);
+				systm.out.println(heightOfStudents[2]);
+				systm.out.println(heightOfStudents[3]);
+			}	
+		
+	}
+	
+}	
--- DataTypesDecleration.java
+++ DataTypesDecleration.java
@@ -5,37 +5,76 @@ public class DataTypesDecleration{
 		byte totalNumberOfTeachars = 30;
 		byte totalNumberOfClasses = 10;
 		byte totalNumberOfSubjcts = 6;
-		byte totalNumberOfStaff = 10;
+		byte totalNumberOfStaff = 10; 
+		
+		System.out.println(totalNumberOfStudents);
+		System.out.println(totalNumberOfClasses);
+		System.out.println(totalNumberOfTeachars);
+		System.out.println(totalNumberOfSubjcts);
+		System.out.println(totalNumberOfStaff);
 		
 		short numberOfWaches = 5;
 		short numberOfBottles=10;
+		
+		System.out.println(numberOfWaches);
+		System.out.println(numberOfBottles);
+		
 		
 		int numberOfBooks=500;
 		int numberofPens=20;
 		int numberofPencils=15;
 		int numberofRooms=10;
 		
-		long phoneNumber=123556789;
-		long adharNumber=5543552365;
+		System.out.println(numberOfBooks);
+		System.out.println(numberofPens);
+		System.out.println(numberofPencils);
+		System.out.println(numberofRooms);
+		
+		
+		long phoneNumber=123556789L;
+		long adharNumber=5543552365L;
+		
+		System.out.println(phoneNumber);
+		System.out.println(adharNumber);
 		
 		
 		float degreePercentage=80.5f;
 		float sslcPercentage=87.2f;
 		float puPercentage=60f;
 		
+		System.out.println(degreePercentage);
+		System.out.println(sslcPercentage);
+		System.out.println(puPercentage);
+		
+		
 		double priceOfRice=12.5455;
 		double priceOfWheate=87.995;
+		
+		System.out.println(priceOfRice);
+		System.out.println(priceOfWheate);
+		
 		
 		char grade='a';
 		char gender='f';
 		char result='p';
 		
+		System.out.println(grade);
+		System.out.println(gender);
+		System.out.println(result);
+		
 		boolean isRight=true;
 		boolean colourIsRed=false;
-		boolean iAm good=true;
+		boolean iAmGood=true;
+		
+		System.out.println(isRight);
+		System.out.println(colourIsRed);
+		System.out.println(iAmGood);
 		
 		String myName="priya";
 		String MySisName="nayana";
+		
+		System.out.println(myName);
+		System.out.println(MySisName);
 		
 		
 		
