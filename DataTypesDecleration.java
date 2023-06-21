5311a15c38ff736195ed26ccbeaf0b5248e3fbca
unknown <you@example.com>
2023-06-21 09:46:12 +0530

    second

--- DataTypesDecleration.java
+++ DataTypesDecleration.java
@@ -1,6 +1,47 @@
 public class DataTypesDecleration{
 	public static void main(String[] args){
 		
+		byte totalNumberOfStudents = 120;
+		byte totalNumberOfTeachars = 30;
+		byte totalNumberOfClasses = 10;
+		byte totalNumberOfSubjcts = 6;
+		byte totalNumberOfStaff = 10;
+		
+		short numberOfWaches = 5;
+		short numberOfBottles=10;
+		
+		int numberOfBooks=500;
+		int numberofPens=20;
+		int numberofPencils=15;
+		int numberofRooms=10;
+		
+		long phoneNumber=123556789;
+		long adharNumber=5543552365;
+		
+		
+		float degreePercentage=80.5f;
+		float sslcPercentage=87.2f;
+		float puPercentage=60f;
+		
+		double priceOfRice=12.5455;
+		double priceOfWheate=87.995;
+		
+		char grade='a';
+		char gender='f';
+		char result='p';
+		
+		boolean isRight=true;
+		boolean colourIsRed=false;
+		boolean iAm good=true;
+		
+		String myName="priya";
+		String MySisName="nayana";
+		
+		
+		
+		
+		
+		
 		
 	}
 }
