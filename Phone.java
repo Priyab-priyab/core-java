2d3830999cd3b478daff23e2a6333f6d6b2049df
unknown <you@example.com>
2023-07-04 20:31:00 +0530

    string-type

---
+++ Phone.java
@@ -0,0 +1,135 @@
+public class Phone{
+	public static void main(String[] args){
+		String brand ="oppo";
+		String name="oppof11";
+		String processor="i3";
+		String colour="blue";
+		String warraanty="three";
+		String weight="five hundread gram";
+		String chargerType ="cPin";
+		String ram = "8 gb ram";
+		String storage = "128 gb";
+		String securityType = "password,facelock";
+		
+		String brand2 =new String("vivo");
+		String name2=new String("vivo y 21");
+		String processor2=new String("i4");
+		String colour2=new String("red");
+		String warraanty2=new String("four");
+		String weight2=new String("250 hundread gram");
+		String chargerType2 =new String("C Pin");
+		String ram2 = new String("8 gb ram");
+		String storage2= new String("128 gb");
+		String securityType2 = new String("fingrPrint,pattern");
+		
+		System.out.println(brand);
+		System.out.println(name);
+		System.out.println(processor);
+		System.out.println(colour);
+		System.out.println(warraanty);
+		System.out.println(weight);
+		System.out.println(chargerType);
+		System.out.println(ram);
+		System.out.println(storage);
+		System.out.println(securityType);
+		
+		System.out.println(brand2);
+		System.out.println(name2);
+		System.out.println(processor2);
+		System.out.println(colour2);
+		System.out.println(warraanty2);
+		System.out.println(weight2);
+		System.out.println(chargerType2);
+		System.out.println(ram2);
+		System.out.println(storage2);
+		System.out.println(securityType2);
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
